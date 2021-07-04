6ba64a772456cfb063dcdfc6d99eca2dc33480ae
rabeeajahangir <69980221+rabeeajahangir@users.noreply.github.com>
2021-07-04 18:59:13 -0400

    add heroku

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 const express = require('express')
+const PORT = process.env.PORT || 3001;
 const app = express()
 const fs = require('fs')
+const path = require('path')
+app.use(express.static('public'))
+app.use(express.urlencoded({extended: true}))
+app.use(express.json())
+const {notes} = require('./db/db.json')
 
-
-const saveNote = new 
 
 
 //post a new note
@@ -13,12 +17,15 @@ app.post('/api/notes', (req, res) => {
     saveNote(newNote)
     res.json(newNote)
 });
-
-
-
-
-
-//notes in json
+//landing page display
+app.get('/', (req,res) => {
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+});
+//notes display, add new note page
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public/notes.html'))
+});
+//list all notes in json
 app.get('/api/notes', (req, res) => {
     res.json(notes)
 })
@@ -35,6 +42,8 @@ app.get('/notes/:id', (req,res) => {
 })
 
 
+
+
 app.listen(PORT, () => {
-    console.log('API server now on port ${PORT}');
+    console.log('API server now on port 3000');
 })
